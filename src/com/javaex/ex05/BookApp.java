33629760d7d209ca601e86f33264207f7a79318f
hi <hi@DESKTOP-SNR6604>
2021-07-02 17:38:08 +0900

    07/02Class(Dao,Vo)

---
+++ src/com/javaex/ex05/BookApp.java
@@ -0,0 +1,28 @@
+package com.javaex.ex05;
+
+public class BookApp {
+
+	public static void main(String[] args) {
+		
+		//(1)
+		//작가테이블 책테이블 완성
+		
+		//작가테이블 시퀀스, 책테이블 시퀀스 완성
+		
+		//authorDao.authorInsert(); 를 사용해 작가6명 데이터 추가
+		
+		//bookDao.bookInsert(); 를 사용해 책8개 데이터 추가
+		
+		//책수정, 삭제 <-- 는 각자 알아서 테스트
+		//책 리스트 출력////////////////////////////////
+		
+		//(2)
+		//스캐너를 통해 사용자가 입력하는 값으로 테이블 검색
+		//"검색어를 입력해 주세요."
+		//"검색 : "
+		//bookDao.getBookList(keyword); --> 4개
+		//검색된 책 정보
+		
+	}
+
+}
---
+++ src/com/javaex/ex05/BookVo.java
@@ -0,0 +1,15 @@
+package com.javaex.ex05;
+
+public class BookVo {
+
+	private int book_id;
+	private String title;
+	private String pubs;
+	private String pubDate;
+	private int authorId;
+	private String authorName;
+	private String authorDesc;
+	
+	
+	
+}
